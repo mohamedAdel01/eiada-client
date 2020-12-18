c7df05113c8aea74f71b671ee8a475bbf1b9c2cc
mohamedadel01 <mohamed7adel96@gmail.com>
2020-12-19 00:13:33 +0200

    edit

---
+++ components/forms/users-forms/add-user.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-form
+    class="user-form col-3 p-3 bg-white rounded"
+    @submit.prevent
+  >
+    <div class="d-flex justify-content-center mb-5">
+      <div class="rounded-circle shadow-sm pointer p-4">
+        <inline-svg
+          fill="#aaa"
+          width="50px"
+          height="50px"
+          :src="require('@/static/images/add-user.svg')"
+        ></inline-svg>
+      </div>
+    </div>
+    <div>
+      <b-form-group :data-label="$t('Email')">
+        <b-form-input
+          v-model="$v.form.email.$model"
+          class="py-4 px-1 border-bottom"
+          :placeholder="$t('Enter', { input: $t('Email') })"
+        ></b-form-input>
+        <b-form-invalid-feedback v-show="!$v.form.email.required">{{
+          $t("This field is required")
+        }}</b-form-invalid-feedback>
+        <b-form-invalid-feedback v-show="!$v.form.email.email">{{
+          $t("Please add a valid email")
+        }}</b-form-invalid-feedback>
+      </b-form-group>
+    </div>
+  </b-form>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, email } from "vuelidate/lib/validators";
+export default {
+  mixins: [validationMixin],
+  data() {
+    return {
+      form: {
+        email: "",
+      },
+    };
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        email,
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/app/users/index.vue
+++ pages/app/users/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>users page</div>
+  <div>
+    <div class="mb-4 d-flex justify-content-between align-items-center">
+      <div class="d-flex align-items-center">
+        <div class="rounded bg-white shadow-sm pointer py-2 px-3">
+          <inline-svg
+            fill="#777"
+            width="30px"
+            height="30px"
+            :src="require('@/static/images/users.svg')"
+          ></inline-svg>
+        </div>
+        <h1 class="mx-2">Users</h1>
+      </div>
+      <div
+        class="d-flex align-items-center tooltip-bottom"
+        :data-label="$t('Add User')"
+      >
+        <button class="rounded btn btn-info shadow-sm pointer py-2 px-3 mx-2">
+          <inline-svg
+            fill="#fff"
+            width="25px"
+            height="20px"
+            :src="require('@/static/images/add-user.svg')"
+          ></inline-svg>
+        </button>
+      </div>
+    </div>
+    <div>
+      
+    </div>
+  </div>
 </template>
 <script>
 export default {
