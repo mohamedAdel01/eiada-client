b33df831a65c37918ef645da4aa78eac307718ca
mohamedadel01 <mohamed7adel96@gmail.com>
2020-12-19 15:49:12 +0200

    edi

--- pages/app/users/index.vue
+++ pages/app/users/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="mb-4 d-flex justify-content-between align-items-center">
       <div class="d-flex align-items-center">
-        <div class="rounded bg-white shadow-sm pointer py-2 px-3">
+        <div class="rounded bg-white shadow-sm pointer py-1 px-2">
           <inline-svg
             fill="#777"
             width="30px"
